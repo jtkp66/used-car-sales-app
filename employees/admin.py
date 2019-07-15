812a89f38e51a615b235cedf61eac86ab1848676
Jamie Pham <jtkp66@gmail.com>
2019-07-15 14:56:38 -0700

    Customized admin display data

--- employees/admin.py
+++ employees/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Employee
 
-admin.site.register(Employee)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(Employee, EmployeeAdmin)
--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'employee', 'year')
+    list_display_links = ('id', 'title')
+    list_filter = ('employee',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'make', 'model', 'year', 'price')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
