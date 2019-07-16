d35c215a65c73455a4773ffc5e4fce12c9baf207
Jamie Pham <jtkp66@gmail.com>
2019-07-16 09:15:58 -0700

    Added Accounts app ad urls for authentication

--- jpauto/settings.py
+++ jpauto/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'employees.apps.EmployeesConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
