f6132befe578a6ba034ce78013b89ada4a86eab7
Jamie Pham <jtkp66@gmail.com>
2019-07-15 23:28:44 -0700

    Refactored home page to display dynamic content

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from employees.models import Employee
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    employees = Employee.objects.order_by('-hire_date')
+
+    mvp_employees = Employee.objects.all().filter(is_mvp=True)
+
+    context = {
+        'employees': employees,
+        'mvp_employees': mvp_employees
+    }
+    return render(request, 'pages/about.html', context)
