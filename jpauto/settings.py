ca81b1c33718772a0c5bbc70cf9d262e3623e88f
Jamie Pham <jtkp66@gmail.com>
2019-07-31 11:38:11 -0700

    Added local settings import

--- jpauto/settings.py
+++ jpauto/settings.py
@@ -146,6 +146,11 @@ MESSAGE_TAGS = {
 # Email cofig
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER='jtkp66@gmail.com'
-EMAIL_HOST_PASSWORD=''
-EMAIL_USE_TLS=True
+EMAIL_HOST_USER = 'jtkp66@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
